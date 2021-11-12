b37e94535e4baf6bac8ecfe88495c96e8fb142e5
vanessaruama <vanessaruama@gmail.com>
2021-11-12 14:15:37 -0300

    merge de alterações

--- Projeto-Equity-Front/src/js/script.js
+++ Projeto-Equity-Front/src/js/script.js
@@ -8,6 +8,8 @@ onload = function() {
         fieldpassword.value = window.localStorage.getItem("exampleInputPassword1")
         fieldsavepsw.checked = window.localStorage.getItem("exampleCheck1")
     }
+
+    console.log("ONLOAD FEITO")
 }
 
 function doLogin() {   
@@ -22,11 +24,50 @@ function doLogin() {
         //Cria/Salva no localStorage
         window.localStorage.setItem("exampleInputEmail1",  fieldlogin.value)
         window.localStorage.setItem("exampleInputPassword1",  fieldpassword.value)
-        window.localStorage.setItem("exampleCheck1",  fieldpassword.checked)
+        window.localStorage.setItem("exampleCheck1",  true)
     } else {
         //Remove do localStorage
         window.localStorage.removeItem("exampleInputEmail1")
         window.localStorage.removeItem("exampleInputPassword1")
         window.localStorage.removeItem("exampleCheck1")
+    }
+
+    doURLAuthentication(fieldlogin.value, fieldpassword.value)
+}
+
+//teste
+function doGetRequest(url) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+  
+function doURLAuthentication(login, senha) {
+    var validuser
+    var urlrequest
+
+    urlrequest = "https://localhost:44397/api/Candidato/CheckarLogin/"
+    urlrequest += login + "&" + senha
+
+    validuser = doGetRequest(urlrequest)
+
+    if (validuser == "true") {
+        console.log("Login realizado com sucesso")
+        // window.open('http://127.0.0.1:5500/index.html#')
+        window.location.href = 'http://127.0.0.1:5500/index.html#'
+    } else {
+        console.log("Login inválido")
+    }
+}
+
+function showPass() {
+
+    if (document.getElementById('exampleInputPassword1').type == "password") {
+        document.getElementById('exampleInputPassword1').type = 'text';
+        document.querySelector(".mostrarsenha").textContent = "Ocultar senha"
+    } else {
+        document.getElementById('exampleInputPassword1').type = 'password';
+        document.querySelector(".mostrarsenha").textContent = "Mostrar senha"
     }
 }
