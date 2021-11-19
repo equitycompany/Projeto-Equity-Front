e71eae090ea389c424275cc4b04eeab91d1ba59b
vanessaruama <vanessaruama@gmail.com>
2021-11-19 14:32:49 -0300

    adicionar redefinição de senha

--- Projeto-Equity-Front/src/js/script.js
+++ Projeto-Equity-Front/src/js/script.js
@@ -1,22 +1,15 @@
 onload = function() {
-    if (window.localStorage.getItem("loginsuccess") == "true") {
-        window.location.href = 'http://127.0.0.1:5500/index.html#'
-        console.log("ONLOAD FEITO LOGIN SUCESSO")
-    } else{
-        const fieldlogin = document.getElementById("exampleInputEmail1")
-        const fieldpassword = document.getElementById("exampleInputPassword1")
-        const fieldsavepsw = document.getElementById("exampleCheck1")
-        
-        if (window.localStorage.getItem("exampleCheck1")) {
-            fieldlogin.value = window.localStorage.getItem("exampleInputEmail1")
-            fieldpassword.value = window.localStorage.getItem("exampleInputPassword1")
-            fieldsavepsw.checked = window.localStorage.getItem("exampleCheck1")
-        }
-
-        console.log("ONLOAD FEITO LOGIN FALHO")
-    }
+    const fieldlogin = document.getElementById("exampleInputEmail1")
+    const fieldpassword = document.getElementById("exampleInputPassword1")
+    const fieldsavepsw = document.getElementById("exampleCheck1")
     
-    window.localStorage.setItem("loginsuccess",  false)
+    if (window.localStorage.getItem("exampleCheck1")) {
+        fieldlogin.value = window.localStorage.getItem("exampleInputEmail1")
+        fieldpassword.value = window.localStorage.getItem("exampleInputPassword1")
+        fieldsavepsw.checked = window.localStorage.getItem("exampleCheck1")
+    }
+
+    console.log("ONLOAD FEITO")
 }
 
 function doLogin() {   
@@ -42,14 +35,15 @@ function doLogin() {
     doURLAuthentication(fieldlogin.value, fieldpassword.value)
 }
 
-//teste
+//Faz a requisição
 function doGetRequest(url) {
     let request = new XMLHttpRequest()
     request.open("GET", url, false)
     request.send()
     return request.responseText
 }
-  
+
+//Faz a autenticação do usuário
 function doURLAuthentication(login, senha) {
     var validuser
     var urlrequest
@@ -61,16 +55,16 @@ function doURLAuthentication(login, senha) {
 
     if (validuser == "true") {
         console.log("Login realizado com sucesso")
-        window.localStorage.setItem("loginsuccess",  true)
-        window.location.href = 'http://127.0.0.1:5500/index.html#'
+        //window.location.href = 'http://127.0.0.1:5500/index.html#'
+        window.open('http://127.0.0.1:5500/index.html#', "_self", false)
+        window.location.replace("http://127.0.0.1:5500/index.html#")
     } else {
         console.log("Login inválido")
-        window.localStorage.setItem("loginsuccess",  false)
     }
 }
 
+//Mostra ou oculta a senha
 function showPass() {
-
     if (document.getElementById('exampleInputPassword1').type == "password") {
         document.getElementById('exampleInputPassword1').type = 'text';
         document.querySelector(".mostrarsenha").textContent = "Ocultar senha"
@@ -80,6 +74,7 @@ function showPass() {
     }
 }
 
+//Envia e-mail do HELP
 function sendEmail() {
     var validuser
     var urlrequest
@@ -96,5 +91,47 @@ function sendEmail() {
         console.log("E-mail enviado")
     } else {
         console.log("E-mail não foi enviado")
+    }
+}
+
+//Envia e-mail para troca de senha
+function sendEmailSenha() {
+    var validuser
+    var urlrequest
+    const email = document.getElementById("emailinput").value
+    urlrequest = "https://localhost:44397/api/Email/TrocadeSenha/"
+    urlrequest += email
+
+    validuser = doGetRequest(urlrequest)
+
+    if (validuser == "true") {
+        console.log("E-mail para recuperação de senha enviado")
+    } else {
+        console.log("E-mail para recuperação de senha não foi enviado")
+    }
+}
+
+//Realiza a troca de senha
+function changepass() {
+    var senha = document.getElementById("inputsenha1").value
+    var confirmesenha = document.getElementById("inputsenha2").value
+
+    if (senha == confirmesenha){
+        var urlchgsenha = window.location.href
+        var chgsenha = urlchgsenha.substring(urlchgsenha.indexOf("_") + 1)
+        console.log(chgsenha)
+        urlrequest = "https://localhost:44397/api/Candidato/AlterarSenha/"
+        urlrequest += chgsenha + "&" + senha
+
+        validuser = doGetRequest(urlrequest)
+
+        if (validuser == "true") {
+            alert("Senha alterada com sucesso")
+            //window.location.href = "http://127.0.0.1:5500/login-candidato.html"
+        } else {
+            alert("Senha não foi alterada")
+        }
+    } else {
+        alert("Digite as duas senhas corretamente")
     }
 }
